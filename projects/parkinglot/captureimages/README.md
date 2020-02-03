e4a7216ff3332dd9e47ea020de05fe45359bac26
Matt Pitlyk <mpitlyk@1904labs.com>
2020-02-02 20:51:37 -0600

    Rearranged folder structure. Give each component its own folder.

---
+++ projects/parkinglot/captureimages/README.md
@@ -0,0 +1,5 @@
+Scripts that run on a Raspberry Pi (or other camera enabled device) to capture images.
+
+`capture.py`: Script to take photo and upload it to Azure blob.
+
+`post_id.py`: Script to post current network IP to a slack channel. Not necessary for `capture.py`, but it was useful for initially setting up the Raspberry Pi's.
--- projects/parkinglot/captureimages/capture.py
+++ projects/parkinglot/captureimages/capture.py
@@ -1,10 +1,13 @@
 import time
 import azure.storage.blob as blb
 
+
 conn_str = 'azure connection string'
 container_name = 'images'
 
 SLEEP = 5
+
+ROOM_NAME = 'RM19'
 
 from io import BytesIO
 from time import sleep
@@ -13,12 +16,14 @@ from picamera import PiCamera
 import sys
 print(sys.version_info)
 
+# Initialize connection to onboard camera.
+camera = PiCamera()
+
+counter = 0
+
 while True:
     # Create the in-memory stream
     stream = BytesIO()
-    camera = PiCamera()
-#    camera.start_preview()
-#    sleep(2)
     camera.capture(stream, format='jpeg')
     time_now = time.ctime().replace(' ', '_')
 
@@ -26,13 +31,10 @@ while True:
     stream.seek(0)
 
     # Create blob client
-    blob_name = f'parkinglot_{time_now}.jpeg'
+    blob_name = f'parkinglot_{ROOM_NAME}_{time_now}.jpeg'
     blob_client = blb.BlobClient.from_connection_string(conn_str=conn_str, container_name=container_name, blob_name=blob_name)
 
     # Save image to blob
     blob_client.upload_blob(stream)
 
     print(f'file {blob_name} uploaded.')
-
-    # Sleep
-    time.sleep(SLEEP * 60)
